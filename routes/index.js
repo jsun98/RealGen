0a9556b305873b840405aa82b8c23698a5a4a389
Josh Sun <joshuasun1998@icloud.com>
2017-09-02 13:20:17 -0400

    setup project to use ejs

--- routes/index.js
+++ routes/index.js
@@ -18,26 +18,36 @@
  * http://expressjs.com/api.html#app.VERB
  */
 
-var keystone = require('keystone');
-var middleware = require('./middleware');
-var importRoutes = keystone.importer(__dirname);
+var keystone = require('keystone')
+// var middleware = require('./middleware')
 
-// Common Middleware
-keystone.pre('routes', middleware.initLocals);
-keystone.pre('render', middleware.flashMessages);
+var importRoutes = keystone.importer(__dirname)
+
+// Handle 404 errors
+keystone.set('404', (req, res, next) => {
+	res.notfound()
+})
+
+// Handle other errors
+keystone.set('500', (err, req, res, next) => {
+	var title, message
+	if (err instanceof Error) {
+		message = err.message
+		err = err.stack
+	}
+	res.err(err, title, message)
+})
 
 // Import Route Controllers
-var routes = {
-	views: importRoutes('./views'),
-};
+var routes = { views: importRoutes('./views') }
 
 // Setup Route Bindings
 exports = module.exports = function (app) {
 	// Views
-	app.get('/', routes.views.index);
-	app.all('/contact', routes.views.contact);
+	app.get('/', routes.views.index)
+	app.all('/contact', routes.views.contact)
 
 	// NOTE: To protect a route so that only admins can see it, use the requireUser middleware:
 	// app.get('/protected', middleware.requireUser, routes.views.protected);
 
-};
+}
--- routes/views/index.js
+++ routes/views/index.js
@@ -1,14 +1,4 @@
-var keystone = require('keystone');
-
 exports = module.exports = function (req, res) {
+	res.render('index')
 
-	var view = new keystone.View(req, res);
-	var locals = res.locals;
-
-	// locals.section is used to set the currently selected
-	// item in the header navigation.
-	locals.section = 'home';
-
-	// Render the view
-	view.render('index');
-};
+}
